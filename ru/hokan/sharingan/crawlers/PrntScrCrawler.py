78c06d6ecca76a71cb1ce19e35a2f5659d91383a
Anton_Achkasov <achkasov.anton.92@gmail.com>
2016-07-11 12:05:49 +0300

    fixed imgur scanner file names resolution

--- ru/hokan/sharingan/crawlers/PrntScrCrawler.py
+++ ru/hokan/sharingan/crawlers/PrntScrCrawler.py
@@ -16,7 +16,7 @@ class PrntScrCrawler(Crawler):
         Crawler.__init__(self, self.__TARGET_URL, self.__OUTPUT_DIRECTORY)
 
     def _get_random_image_name(self):
-        return ''.join(random.choice(string.lowercase + string.digits) for i in range(self.__IMAGE_NAME_LENGTH))
+        return ''.join(random.choice('abcdef' + string.digits) for i in range(self.__IMAGE_NAME_LENGTH))
 
     def _should_image_be_processed(self, full_image_url):
         try:
