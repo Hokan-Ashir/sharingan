1c15f06e80da75586fc9b8e75ac4cf498029564c
hokan <achkasov.anton.92@gmail.com>
2016-10-11 00:01:56 +0300

    myshared-crawler improved a bit for approximately 25 images from 1000 requests

--- ru/hokan/sharingan/crawlers/MySharedCrawler.py
+++ ru/hokan/sharingan/crawlers/MySharedCrawler.py
@@ -5,9 +5,9 @@ from ru.hokan.sharingan.common.crawler import Crawler
 
 
 class MySharedScrCrawler(Crawler):
-    __FIRST_URL_NUMBER_PART_LENGTH = 2
+    __FIRST_URL_NUMBER_TOP_NUMBER = 17
     __SECOND_URL_NUMBER_PART_LENGTH = 7
-    __THIRD_URL_NUMBER_PART_LENGTH = 2
+    __THIRD_URL_NUMBER_TOP_NUMBER = 30
     __OUTPUT_DIRECTORY = '.' + os.path.sep + 'output_myshared' + os.path.sep
     __TARGET_URL = 'http://player.myshared.ru/'
 
@@ -15,11 +15,11 @@ class MySharedScrCrawler(Crawler):
         Crawler.__init__(self, self.__TARGET_URL, self.__OUTPUT_DIRECTORY, should_text_be_extracted)
 
     def _get_random_url_name(self):
-        return str(random.randint(0, 10 ** self.__FIRST_URL_NUMBER_PART_LENGTH)) \
+        return str(random.randint(1, self.__FIRST_URL_NUMBER_TOP_NUMBER)) \
                + "/" \
-               + str(random.randint(0, 10 ** self.__SECOND_URL_NUMBER_PART_LENGTH)) \
+               + str(random.randint(10 ** (self.__SECOND_URL_NUMBER_PART_LENGTH - 2), 10 ** self.__SECOND_URL_NUMBER_PART_LENGTH)) \
                + "/data/images/img" \
-               + str(random.randint(0, 10 ** self.__THIRD_URL_NUMBER_PART_LENGTH)) \
+               + str(random.randint(1, self.__THIRD_URL_NUMBER_TOP_NUMBER)) \
                + ".jpg"
 
     def _get_image_url_from_source(self, url_data):
